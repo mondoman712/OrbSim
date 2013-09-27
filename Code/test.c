4d1d9bdd52aa50da5813fafdc3bc229ebfd733b9
mondoman712 <oommmmmmmo@googlemail.com>
2013-09-27 21:01:48 +0100

    sun

--- Code/test.c
+++ Code/test.c
@@ -17,9 +17,16 @@ int main(int argc, const char *argv[])
 
 	struct obj {
 		char name[MAXLEN];
-		long mass;
+		long double mass;
 		struct xyz pos;
 		struct velocity v; 
 	};
+
+	struct obj sun = {"Sun", 1.9891e30, 0, 0, 0, 0, 0, 0, 0};
+
+	printf("%s\n", sun.name);
+	printf("%g\n", sun.mass);
+	printf("%d\n", sun.pos.x);
+
 	return 0;
 }
