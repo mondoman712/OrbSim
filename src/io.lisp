4c20fdc5533c7458ae99e3c5ddb0311e95752cda
mondoman712 <oommmmmmmo@googlemail.com>
2014-03-05 19:17:59 +0000

    writes to file

--- src/io.lisp
+++ src/io.lisp
@@ -1,10 +1,16 @@
-(defun get-slots (object)
-  (mapcar #'sb-pcl:slot-definition-name (sb-pcl:class-slots (class-of object))))
+(defun body-to-list (body)
+  (list :pos-x (x (pos body))
+	:pos-y (y (pos body))
+	:vel-x (x (vel body))
+	:vel-y (y (vel body))
+	:mass (mass body)
+	:size (size body)
+	:colour-r (sdl:r (colour body))
+	:colour-g (sdl:g (colour body))
+	:colour-b (sdl:b (colour body))))
 
-(defun printable-object (object)
-  (cons (class-of object) 
-	(loop for slot in (get-slots object)
-	   collecting (slot-value object slot)))
+(defun bodies-to-list (bodies)
+  (mapcar #'body-to-list bodies))
 
 (defun save-list (lst filename)
   (with-open-file (out filename
@@ -12,9 +18,6 @@
 		       :if-exists :supersede)
     (with-standard-io-syntax
       (print lst out))))
-
-(defun save-obj-list (lst filename)
-  (save-list (mapcar #'printable-object lst) filename))
 
 (defun read-list (filename)
   (with-open-file (in filename)
